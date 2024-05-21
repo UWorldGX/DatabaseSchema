757bfd3ec67c842f991b7312127f098889258cb3
Flash <huyantaor@126.com>
2024-05-21 20:54:58 +0800

    v0.1 Project Template for Information Security Learning

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,54 +9,52 @@ using System.Net.Http;
 using System.Text;
 using System.Windows;
 
-namespace FirewallDemo
+namespace FirewallDemo;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private readonly ServiceProvider _serviceProvider;
+    private IConfigurationRoot root = null!;
+
+    //1 封装构造函数
+    public App()
     {
-        private readonly ServiceProvider _serviceProvider;
-        private IConfigurationRoot root = null!;
-
-        //1 封装构造函数
-        public App()
-        {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            ServiceCollection services = new ServiceCollection();
-            ConfigureServices(services);
-            _serviceProvider = services.BuildServiceProvider();
-        }
-
-        //2 封装服务注入
-        private void ConfigureServices(ServiceCollection services)
-        {
-            var builder = new ConfigurationBuilder();
-            services.AddSingleton<HttpClient>();
-            //using HttpClient httpClient = new();
-            //HttpRequestMessage request = new(HttpMethod.Get, "http://150.158.40.245");
-            //builder.AddJsonStream(httpClient.Send(request).Content.ReadAsStream());
-            //builder.AddJsonFile("config.json", optional: false, reloadOnChange: true);
-            //root = builder.Build();
-
-            //services.AddOptions()
-            //    .Configure<Model.MemoManager>(root.Bind);
-            //services.AddDbContextFactory<InnoContext>(option =>
-            //{
-            //    option.UseMySql(System.Configuration.ConfigurationManager.ConnectionStrings["connStr"].ConnectionString, new MySqlServerVersion(new Version(8, 0, 35)))
-            //    .EnableDetailedErrors();
-            //}, ServiceLifetime.Scoped);
-            services.AddSingleton<MainVM>();
-
-            services.AddScoped<MainWindow>();
-        }
-
-        //3 重写OnStartUp函数
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            var window = _serviceProvider.GetRequiredService<MainWindow>();
-            window.Show();
-        }
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        ServiceCollection services = new ServiceCollection();
+        ConfigureServices(services);
+        _serviceProvider = services.BuildServiceProvider();
     }
 
+    //2 封装服务注入
+    private void ConfigureServices(ServiceCollection services)
+    {
+        var builder = new ConfigurationBuilder();
+        services.AddSingleton<HttpClient>();
+        //using HttpClient httpClient = new();
+        //HttpRequestMessage request = new(HttpMethod.Get, "http://150.158.40.245");
+        //builder.AddJsonStream(httpClient.Send(request).Content.ReadAsStream());
+        //builder.AddJsonFile("config.json", optional: false, reloadOnChange: true);
+        //root = builder.Build();
+
+        //services.AddOptions()
+        //    .Configure<Model.MemoManager>(root.Bind);
+        //services.AddDbContextFactory<InnoContext>(option =>
+        //{
+        //    option.UseMySql(System.Configuration.ConfigurationManager.ConnectionStrings["connStr"].ConnectionString, new MySqlServerVersion(new Version(8, 0, 35)))
+        //    .EnableDetailedErrors();
+        //}, ServiceLifetime.Scoped);
+        services.AddSingleton<MainVM>();
+
+        services.AddScoped<MainWindow>();
+    }
+
+    //3 重写OnStartUp函数
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var window = _serviceProvider.GetRequiredService<MainWindow>();
+        window.Show();
+    }
 }
--- ViewModel/MainVM.cs
+++ ViewModel/MainVM.cs
@@ -1,13 +1,23 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using FirewallDemo.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FirewallDemo.ViewModel
+namespace FirewallDemo.ViewModel;
+
+public partial class MainVM : ObservableObject
 {
-    public partial class MainVM : ObservableObject
+    [ObservableProperty]
+    private PacketFilterFirewall _firewall = new(IPAddress.Parse("127.0.0.1"), 8888);
+
+    [RelayCommand]
+    private void StartListening()
     {
+        _firewall.Filter();
     }
 }
